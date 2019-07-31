eccd03be5065bf03e00b9dc037c4eba0a4d5c892
Silas <1029540+silasl@users.noreply.github.com>
2019-07-31 17:16:03 +0100

    Corrected path update

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   redirectRoot: 'https://design.tax.service.gov.uk',
   pathOverrides: {
-    'ask-for-consent': 'ask-user-consent',
+    'ask-user-consent': 'ask-for-consent',
     'open-in-a-new-window': 'new-tab-link'
   }
 }
